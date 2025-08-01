e582962b38a290e954ed8cb3b3211cd1620b24a9
ericmariot <eric.mariot@logrock.com>
2025-08-01 14:33:59 -0300

    feat: add docker-compose to run dd agent locally

--- lambda_function.py
+++ lambda_function.py
@@ -1,9 +1,18 @@
 from typing import Any, Dict
+import logging
 
 from datadog_lambda.wrapper import datadog_lambda_wrapper
 
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 @datadog_lambda_wrapper
 def lambda_handler(event: Dict[str, Any], context) -> Dict[str, Any]:
-    print("Processing event: %s", event)
-    return
+    logger.info("Processing event", extra={"event": event})
+    
+    print(f"Processing event: {event}")
+    
+    return {
+        "statusCode": 200,
+        "body": "Event processed successfully"
+    }
